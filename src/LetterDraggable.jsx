f4ccf6b75125eacd743c19710149907eb9ec62e7
David Chen <davidc3287@gmail.com>
2022-12-01 17:56:01 -0800

    draggable straight arrows

--- src/LetterDraggable.jsx
+++ src/LetterDraggable.jsx
@@ -1,34 +1,30 @@
-import React, { useRef } from "react";
-import { useDrag } from "react-dnd";
-import Xarrow from "react-xarrows";
+import React, { useRef, useState } from "react";
+import Draggable from "react-draggable";
+import Xarrow, { useXarrow } from "react-xarrows";
 
 const LetterDraggable = ({ startNodeID }) => {
   const draggableRef = useRef(null);
-  const [{ isDragging }, drag] = useDrag(
-    () => ({
-      type: "invisible-dragger",
-      collect: (monitor) => ({
-        isDragging: !!monitor.isDragging(),
-      }),
-    }),
-    []
-  );
+  const [isDragging, setIsDragging] = useState(false);
+  const updateXArrow = useXarrow();
 
   return (
     <>
-      <div
-        ref={drag}
-        style={{
-          width: "25",
-          height: "25px",
-          backgroundColor: "rgb(135, 200, 200)",
-          borderRadius: "25%",
-          opacity: isDragging ? 0.5 : 1,
-        }}
-      >
-        <p ref={draggableRef}>drag</p>
-      </div>
-      {isDragging && <Xarrow start={startNodeID} end={draggableRef} />}
+      <Draggable onStart={() => setIsDragging(true)} onDrag={updateXArrow}>
+        <div
+          style={{
+            width: "25",
+            height: "25px",
+            backgroundColor: "rgb(135, 200, 200)",
+            borderRadius: "25%",
+            opacity: isDragging ? 0.5 : 1,
+          }}
+        >
+          <p ref={draggableRef}>drag</p>
+        </div>
+      </Draggable>
+      {isDragging && (
+        <Xarrow start={startNodeID} end={draggableRef} path="straight" />
+      )}
     </>
   );
 };
