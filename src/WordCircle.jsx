1394c262e35de2817373a0a9c068b443ef17c1c0
David Chen <davidc3287@gmail.com>
2022-12-07 21:08:33 -0800

    words in a circle

--- src/WordCircle.jsx
+++ src/WordCircle.jsx
@@ -5,11 +5,12 @@ import { disableBodyScroll } from "body-scroll-lock";
 
 const WordCircle = () => {
   const [dragging, setDragging] = useState(false);
-  const letters = ["A", "B", "C"];
+  const letters = Array.from("NABISCO");
   const [arrows, setArrows] = useState([]);
   // const [lettersArray, setLettersArray] = useState([]);
 
   disableBodyScroll(document.body);
+
   const onDragEnd = () => {
     if (arrows.length > 0) {
       let lettersArray = [];
@@ -23,9 +24,50 @@ const WordCircle = () => {
     }
   };
 
+  const getCircleLayout = (letters) => {
+    let radius = 100;
+    return (
+      <div
+        style={{
+          // light grey background
+          backgroundColor: "rgba(205, 209, 230, 0.3)",
+          minWidth: radius * 2 + 75,
+          width: radius * 2 + 75,
+          height: "same-as-width",
+          minHeight: radius * 2 + 75,
+          borderRadius: "50%",
+        }}
+      >
+        {letters.map((letter, i) => {
+          let angle = (2 * Math.PI) / letters.length;
+          // iniital value centered vertically on positive y-axis
+          let x = radius * Math.cos(angle * i - Math.PI / 2) + radius;
+          let y = radius * Math.sin(angle * i - Math.PI / 2) + radius;
+          return (
+            <div
+              key={`letter${i}`}
+              // className="column"
+              style={{ left: `${x}px`, top: `${y}px`, position: "absolute" }}
+            >
+              <LetterNode
+                id={`letter${i}_${letter}`}
+                letter={letter}
+                letterID={`letter${i}_${letter}`}
+                arrows={arrows}
+                setArrows={setArrows}
+                onDragEnd={onDragEnd}
+              />
+            </div>
+          );
+        })}
+      </div>
+    );
+  };
+
   return (
     <>
-      <div className="columns m-5">
+      {getCircleLayout(letters)}
+      {/* <div className="columns m-5">
         {letters.map((letter, i) => (
           <div key={`letter${i}`} className="column">
             <LetterNode
@@ -52,7 +94,7 @@ const WordCircle = () => {
             />
           </div>
         ))}
-      </div>
+      </div> */}
       {arrows.map((arrow, i) => (
         <Xarrow
           start={arrow.start}
@@ -61,6 +103,7 @@ const WordCircle = () => {
           path="straight"
           startAnchor="middle"
           endAnchor="middle"
+          // strokeWidth={15}
         />
       ))}
     </>
