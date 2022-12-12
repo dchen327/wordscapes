232f49729f0ef80c878fa8c571928d00fa54975a
David Chen <davidc3287@gmail.com>
2022-12-11 17:40:54 -0800

    render crossword

--- src/Crossword.jsx
+++ src/Crossword.jsx
@@ -17,17 +17,23 @@ const Crossword = () => {
     <>
       <button onClick={fetchCrossword}>Fetch Crossword</button>
       <div className="mx-2 flex items-center justify-center">
-        <table className="table-fixed border">
+        <table className="table-fixed border-separate border">
           <tbody>
             {grid &&
               grid.map((row, i) => (
                 <tr key={i}>
                   {row.map((col, j) => (
                     <td
-                      className={`${col === "-" ? "invisible" : ""}`}
+                      className={`${
+                        col === "-" ? "invisible" : "border rounded"
+                      } `}
                       style={{
                         width: "50px",
                         height: "50px",
+                        backgroundColor:
+                          col === "-"
+                            ? "transparent"
+                            : "rgba(205, 209, 230, 0.3)",
                       }}
                       key={`${i}${j}`}
                     >
