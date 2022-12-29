1c0281c7eb0d63199e1d98d55ca6b71cab134e85
David Chen <davidc3287@gmail.com>
2022-12-28 19:09:25 -0500

    fav icon, word tests

--- api/test.py
+++ api/test.py
@@ -4,6 +4,7 @@ with open('wordnik.txt') as f:
     wordnik = set([word for word in wordnik if 3 <= len(word) <= 9])
     print(len(wordnik))
 
+# %%
 with open('wordscapes_txt.txt') as f:
     words = f.read().splitlines()
     words = [word for word in words if word in wordnik]
@@ -13,3 +14,16 @@ with open('wordscapes_txt.txt') as f:
 with open('clean_words.txt', 'w') as f:
     for word in words:
         f.write(word + '\n')
+
+# %%
+import json
+lst = []
+with open('wiktionary.json', 'r', encoding='utf-8') as f:
+    line_num = 1
+    for line in f:
+        lst.append(json.loads(line))
+        line_num += 1
+        if line_num > 100:
+            break
+
+print(lst[0].keys())
