9d7c713f17bb00447d628e5af234f24308a6b48a
David Chen <davidc3287@gmail.com>
2022-12-29 09:10:53 -0500

    new levels with fewer bonus words

--- api/wordscapes.py
+++ api/wordscapes.py
@@ -61,7 +61,15 @@ def gen_crossword(puzzle_words: List[str], output_file: str):
     del puzzle_words[-1]  # remove longest word
     no_3_letters = [word for word in puzzle_words if len(word) > 3]
     # prioritize adding longer words
-    word_weights = [len(word) - 2.5 for word in no_3_letters]
+    len_to_weight = {
+        4: 1,
+        5: 4,
+        6: 9,
+        7: 16,
+        8: 25,
+        9: 36,
+    }
+    word_weights = [len_to_weight[len(word)] for word in no_3_letters]
 
     words = list(np.random.choice(no_3_letters, size=min(
         len(no_3_letters), 50), replace=False, p=word_weights/np.sum(word_weights)))
@@ -214,7 +222,7 @@ def setup():
         num_crosswords = 100  # will try to generate this many, might be fewer
 
         main_words = random.choices(
-            words_by_len[8] + words_by_len[7], k=2*num_crosswords)
+            words_by_len[8] + words_by_len[7], k=3*num_crosswords)
 
         # clear any previous puzzles
         if os.path.exists('levels'):
@@ -222,32 +230,25 @@ def setup():
         # make puzzles directory
         os.makedirs('levels')
 
-        # level_num = 1
-        # while level_num <= num_crosswords:
-        #     main_word = main_words.pop()
-        #     puzzle_words = gen_puzzle_words(words_by_len, main_word)
-        #     output_file = f'levels/level_{level_num}.txt'
-        #     if gen_crossword(
-        #             puzzle_words, output_file) is not None:
-        #         print(f'Crossword #{level_num}: {main_word}')
-        #         level_num += 1
+        level_num = 1
+        while level_num <= num_crosswords:
+            main_word = main_words.pop()
+            puzzle_words = gen_puzzle_words(words_by_len, main_word)
+            no_3_letters = [word for word in puzzle_words if len(word) > 3]
+            if len(no_3_letters) > 30:  # too many words
+                continue
+            output_file = f'levels/level_{level_num}.txt'
+            if gen_crossword(
+                    puzzle_words, output_file) is not None:
+                print(f'Crossword #{level_num}: {main_word}')
+                level_num += 1
 
-        main_word = random.choice(words_by_len[8])
-        main_word = 'readings'
-        l = []
-        l2 = []
-        for word in random.choices(words_by_len[8] + words_by_len[7], k=1000):
-            puzzle_words = gen_puzzle_words(words_by_len, word)
-            l.append(len(puzzle_words))
-            no_3_ls = [c for c in puzzle_words if len(c) > 3]
-            l2.append(len(no_3_ls))
+        # main_word = random.choice(words_by_len[8])
+        # main_word = 'pulley'
 
-        import pandas as pd
-        print(pd.Series(l).describe(
-            percentiles=[.25, .5, .6, .7, .8, .9, .95, .99]))
-        print(pd.Series(l2).describe(
-            percentiles=[.25, .5, .6, .7, .8, .9, .95, .99]))
         # puzzle_words = gen_puzzle_words(words_by_len, main_word)
+        # print(len(puzzle_words))
+        # print(puzzle_words)
         # gen_crossword(puzzle_words, 'puzzle.txt')
 
 
